166b8cb49b882c955e5b0c049eee101f4d2e2e12
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-02-10 13:46:01 +0300

    Верстка главного меню + toggle 'open-menu' class

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,15 @@ import * as $ from 'jquery';
 
 $(function () {
 	initApp();
+	toggleMainMenu();
 });
 
 function initApp() {
 	console.log('initApp');
+}
+
+function toggleMainMenu() {
+	$('#menu-burger').click(function () {
+		$('html').toggleClass('main-menu-open')
+	})
 }
