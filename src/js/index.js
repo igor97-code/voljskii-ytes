2fbd491db1587e180d550c572506d2636afb1b3e
igor <starodubcevinternetlab.ru>
2020-02-10 16:22:43 +0300

    Изменения блоков

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
 import * as $ from 'jquery';
+import 'slick-carousel';
 
 $(function () {
-	initApp();
+   initApp();
+   toggleMainMenu();
+   oneItemSlider();
 });
 
 function initApp() {
-	console.log('initApp');
+   console.log('initApp');
+}
+
+function toggleMainMenu() {
+   $('#menu-burger').click(function () {
+      $('html').toggleClass('main-menu-open')
+   })
+}
+function oneItemSlider() {
+   $('.initialize-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      vertical: false,
+      arrows: false,
+      dots: true,
+      customPaging : function(slider, i) {
+         return '<a>' + '</a>';
+      }
+   });
 }
